a5deaee48573753e224a0d1fe2387281ec62951b
ShingoYadomoto <shingo.yadomoto@gmail.com>
2023-03-28 19:47:53 +0900

    use Match() for check pattern match

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	// Uncomment this to pass the first stage
 	// "bytes"
 	"fmt"
@@ -49,30 +48,32 @@ func matchLine(line []byte, pattern string) (bool, error) {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	switch pattern {
-	case `\d`:
-		chars := "123456789"
-		ok = bytes.ContainsAny(line, chars)
-	case `\w`:
-		alpha := "abcdefghijklmnopqrstuvwxyz"
-		chars := "123456789" + alpha + strings.ToUpper(alpha) + "_"
-		ok = bytes.ContainsAny(line, chars)
-	default:
-		ok = bytes.ContainsAny(line, pattern)
-	}
+	ok = Match(pattern, string(line))
 
-	if strings.HasPrefix(pattern, "[") && strings.HasSuffix(pattern, "]") {
-		chars := strings.TrimSuffix(strings.TrimPrefix(pattern, "["), "]")
-		negative := false
-		if strings.HasPrefix(chars, "^") {
-			chars = strings.TrimPrefix(pattern, "^")
-			negative = true
-		}
-		ok = bytes.ContainsAny(line, chars)
-		if negative {
-			ok = !ok
-		}
-	}
+	//switch pattern {
+	//case `\d`:
+	//	chars := "123456789"
+	//	ok = bytes.ContainsAny(line, chars)
+	//case `\w`:
+	//	alpha := "abcdefghijklmnopqrstuvwxyz"
+	//	chars := "123456789" + alpha + strings.ToUpper(alpha) + "_"
+	//	ok = bytes.ContainsAny(line, chars)
+	//default:
+	//	ok = bytes.ContainsAny(line, pattern)
+	//}
+	//
+	//if strings.HasPrefix(pattern, "[") && strings.HasSuffix(pattern, "]") {
+	//	chars := strings.TrimSuffix(strings.TrimPrefix(pattern, "["), "]")
+	//	negative := false
+	//	if strings.HasPrefix(chars, "^") {
+	//		chars = strings.TrimPrefix(pattern, "^")
+	//		negative = true
+	//	}
+	//	ok = bytes.ContainsAny(line, chars)
+	//	if negative {
+	//		ok = !ok
+	//	}
+	//}
 
 	return ok, nil
 }
