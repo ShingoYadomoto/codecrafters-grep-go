67a55a22518d4d6e25edf5e926810892f9e94d31
ShingoYadomoto <shingo.yadomoto@gmail.com>
2023-03-28 22:29:42 +0900

    RED: Match one or more times test

--- cmd/mygrep/matcher_test.go
+++ cmd/mygrep/matcher_test.go
@@ -42,6 +42,10 @@ func TestMatch(t *testing.T) {
 
 		{name: "match: dog$", regexp: `dog$`, text: "dog", want: true},
 		{name: "not match: dog$", regexp: `dog$`, text: "dogs", want: false},
+
+		{name: "match: a+", regexp: `a+`, text: "apple", want: true},
+		{name: "match: a+", regexp: `a+`, text: "SaaS", want: true},
+		{name: "not match: a+", regexp: `a+`, text: "dog", want: false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
